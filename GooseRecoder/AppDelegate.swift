3a21a01887e6c2cd09c55b8d53eea0cdd18ac257
krwd1117 <krwd1117@gmail.com>
2022-03-14 14:37:43 +0900

    앱 이름 현지화, 세로 고정

--- GooseRecoder/AppDelegate.swift
+++ GooseRecoder/AppDelegate.swift
@@ -14,9 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        
         LocationManager.shared.startUpdating()
-        
         return true
     }
 
@@ -32,6 +30,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+    
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+        // 세로방향 고정
+        return UIInterfaceOrientationMask.portrait
     }
 
 
